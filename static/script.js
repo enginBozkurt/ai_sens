a642e88902f0aa48011fbf3192f18a17b906e61c
Engin Bozkurt <engin.bozkurt034@gmail.com>
2024-11-22 13:09:05 +0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -25,7 +25,7 @@ async function sendMessage() {
 
     try {
         // Send the message to the Flask server
-        const response = await fetch('http://127.0.0.1:5000/chat', {
+        const response = await fetch('/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
